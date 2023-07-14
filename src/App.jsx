8e31a765a5081c52580d2eb24b2c7df40ca638b0
OrphanKnight <47511552+OrphanKnight@users.noreply.github.com>
2023-07-13 21:58:05 -0400

    Cards styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import "./App.scss";
 
 function App() {
 	const handleTheme = () => {
+		var element = document.getElementById("card-body");
+		element.classList.toggle("dark");
 		document.body.classList.toggle("dark");
 	};
 
 	return (
-		<div>
-			<button
-				className="theme-button"
-				onClick={handleTheme}
-				type="button"
-			>
+		<div id="card-body" className="card-body">
+			<button className="theme-button" onClick={handleTheme} type="button">
 				<svg
 					className="theme-button-icon light"
 					xmlns="http://www.w3.org/2000/svg"
@@ -31,6 +29,62 @@ function App() {
 			</button>
 
 			<div className="music-container">
+				<div className="menu">
+					<MusicPlayer
+						title="Love the Way You Lie ft. Rihanna"
+						composer="Eminem"
+						src={audio}
+						image="https://assets.codepen.io/4175254/ymo.jpg"
+					/>
+					{/* <div className="music-player-container-list">
+						<div class="music-controls-container">
+							<button class="play-button play" type="button">
+								<svg
+									class="play-button-icon play"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 256 256"
+								>
+									<path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path>
+								</svg>
+								<svg
+									class="play-button-icon pause"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 256 256"
+								>
+									<path d="M216,48V208a16,16,0,0,1-16,16H160a16,16,0,0,1-16-16V48a16,16,0,0,1,16-16h40A16,16,0,0,1,216,48ZM96,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16H96a16,16,0,0,0,16-16V48A16,16,0,0,0,96,32Z"></path>
+								</svg>
+							</button>
+
+							<div class="music-title" type="button">
+								Track
+							</div>
+							<button class="music-controls-button skip back" type="button">
+
+
+							</button>
+
+							<button class="music-controls-button skip forward" type="button">
+
+
+							</button>
+							<button
+								class="music-controls-button shuffle toggle"
+								type="button"
+							>
+
+
+							</button>
+						</div>
+					</div> */}
+				</div>
+
+				<MusicPlayer
+					title="Love the Way You Lie ft. Rihanna"
+					composer="Eminem"
+					src={audio}
+					image="https://assets.codepen.io/4175254/ymo.jpg"
+				/>
+
 				<MusicPlayer
 					title="Love the Way You Lie ft. Rihanna"
 					composer="Eminem"
